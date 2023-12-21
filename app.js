53fcbedd8a980cf372379ba7e42333cb9c55ddb3
analaba sylvester <ubachukwusylvester8@gmail.com>
2023-12-21 06:36:36 -0800

    didi some update

--- app.js
+++ app.js
@@ -11,3 +11,56 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+
+
+
+
+
+
+
+// function  addition(num1, num2) {
+//     return num1 + num2
+// }
+
+// function  sub(num1, num2) {
+//     num1 - num2
+// }
+
+// function  div(num1, num2) {
+//     num1 / num2
+// }
+
+// function  mul(num1, num2) {
+//     num1 * num2
+// }
+
+
+
+// let  oOperator = prompt("pick an option. ")
+
+// switch (oOperator) {
+//     case 1:
+//        alert(addition(34, 34))
+//         // result = Number(num1) + Number(num2)
+//         // alert(`the result in addition is ${result}`)
+//         break;
+
+//     case 2:
+//         result = num1 - num2
+//         alert(`the result in subtraction is ${result}`)
+//         break;
+
+//     case 3:
+//         result = num1 / num2
+//         alert(`the result in division is ${result}`)
+//         break;
+//     case 4:
+//         result = num1 * num2
+//         alert(`the result in multiplication is ${result}`)
+//         break;
+
+//         default:
+//             console.log("error")
+//             break
+// }
+
